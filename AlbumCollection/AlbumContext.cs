2ac68aedfe44004fe1e756b89570e4d87dc8ef99
Mary McGeary <malicemcgeary@gmail.com>
2019-03-10 09:02:53 -0400

    AddedSeedData

--- AlbumCollection/AlbumContext.cs
+++ AlbumCollection/AlbumContext.cs
@@ -28,9 +28,23 @@ namespace AlbumCollection
             new Album()
             {
                 AlbumId = 1,
-                AlbumName = "Some Album",
-                ArtistName = "Not Me",
-                AlbumImageLocation = "Somewhere out there"
+                AlbumName = "Jesus Use Me",
+                ArtistName = "The Faith Tones",
+                AlbumImageLocation = "Img/TheFaithTones.jpg"
+            },
+            new Album()
+            {
+                AlbumId = 2,
+                AlbumName = "Mr. Spock's Music From Outer Space",
+                ArtistName = "Leonard Nimoy",
+                AlbumImageLocation = "Img/LeonardNimoy.jpg"
+            },
+            new Album()
+            {
+                AlbumId = 3,
+                AlbumName = "Old McDonald Had a Farm",
+                ArtistName = "Alan Mills and the Four Shipmates",
+                AlbumImageLocation = "Img/AlanMills.jpg"
             }
             );
 
@@ -38,7 +52,32 @@ namespace AlbumCollection
             new Song()
             {
                 SongId = 1,
-                SongName = "Yay!"
+                SongName = "Exposed"
+            },
+            new Song()
+            {
+                SongId = 2,
+                SongName = "It's Different Now"
+            },
+            new Song()
+            {
+                SongId = 3,
+                SongName = "Music to Watch Space Girls By"
+            },
+            new Song()
+            {
+                SongId = 4,
+                SongName = "Twinkle, Twinkle Little Earth"
+            },
+            new Song()
+            {
+                SongId = 5,
+                SongName = "I Know an Old Lady"
+            },
+            new Song()
+            {
+                SongId = 6,
+                SongName = "Froggie Went A-Courtin"
             }
             );
 
---
+++ AlbumCollection/Migrations/20190310130213_AddedSeedData.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AlbumCollection.Migrations
+{
+    public partial class AddedSeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Albums",
+                keyColumn: "AlbumId",
+                keyValue: 1,
+                columns: new[] { "AlbumImageLocation", "AlbumName", "ArtistName" },
+                values: new object[] { "Img/TheFaithTones.jpg", "Jesus Use Me", "The Faith Tones" });
+
+            migrationBuilder.InsertData(
+                table: "Albums",
+                columns: new[] { "AlbumId", "AlbumImageLocation", "AlbumName", "ArtistName" },
+                values: new object[,]
+                {
+                    { 2, "Img/LeonardNimoy.jpg", "Mr. Spock's Music From Outer Space", "Leonard Nimoy" },
+                    { 3, "Img/AlanMills.jpg", "Old McDonald Had a Farm", "Alan Mills and the Four Shipmates" }
+                });
+
+            migrationBuilder.UpdateData(
+                table: "Songs",
+                keyColumn: "SongId",
+                keyValue: 1,
+                column: "SongName",
+                value: "Exposed");
+
+            migrationBuilder.InsertData(
+                table: "Songs",
+                columns: new[] { "SongId", "SongName" },
+                values: new object[,]
+                {
+                    { 2, "It's Different Now" },
+                    { 3, "Music to Watch Space Girls By" },
+                    { 4, "Twinkle, Twinkle Little Earth" },
+                    { 5, "I Know an Old Lady" },
+                    { 6, "Froggie Went A-Courtin" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Albums",
+                keyColumn: "AlbumId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Albums",
+                keyColumn: "AlbumId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Songs",
+                keyColumn: "SongId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Songs",
+                keyColumn: "SongId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Songs",
+                keyColumn: "SongId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Songs",
+                keyColumn: "SongId",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Songs",
+                keyColumn: "SongId",
+                keyValue: 6);
+
+            migrationBuilder.UpdateData(
+                table: "Albums",
+                keyColumn: "AlbumId",
+                keyValue: 1,
+                columns: new[] { "AlbumImageLocation", "AlbumName", "ArtistName" },
+                values: new object[] { "Somewhere out there", "Some Album", "Not Me" });
+
+            migrationBuilder.UpdateData(
+                table: "Songs",
+                keyColumn: "SongId",
+                keyValue: 1,
+                column: "SongName",
+                value: "Yay!");
+        }
+    }
+}
